7ae9f9e223ef5f2092b83c924f21de014a08d4b2
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-04-24 14:42:18 -0500

    Iniciando proyecto, agregando bottomNavigation and foatingActionButton

---
+++ lib/src/pages/direcciones_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DireccionesPage extends StatelessWidget {
+  const DireccionesPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Direcciones Page')),
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: null)
+        ],
+      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: _scanQR,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  _scanQR() {
+    print('Scan QR...');
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+        break;
+      case 1:
+        return DireccionesPage();
+        break;
+      default:
+        return MapasPage();
+    }
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapas')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.location_on), title: Text('Direcciones')),
+      ],
+    );
+  }
+}
